b4359205e06ce4d6cfba8b52e0c6b37a157c0784
Francisco Macedo <francisco.macedo@optimile.eu>
2022-04-02 18:03:50 +0100

    do not restart db session

--- app/api/workflow.py
+++ app/api/workflow.py
@@ -10,24 +10,23 @@ def populate_database(replace):
 
     bot = Networks()
     networks = bot.get()
-    crud.create_networks(next(get_db()), networks)
+    db = next(get_db())
+    crud.create_networks(db, networks)
     for network in networks:
         print("getting stations for", network.id)
 
         stations = Stations(session=bot.session, network_id=network.id).get()
         print("population stations db")
-        crud.create_stations(next(get_db()), stations, network_id=network.id)
+        crud.create_stations(db, stations, network_id=network.id)
 
         for station in stations:
             print("getting parameters for", station.id)
             if not replace:
-                if crud.get_stations_parameters(
-                    next(get_db()), station_ids=[station.id]
-                ):
+                if crud.get_stations_parameters(db, station_ids=[station.id]):
                     continue
 
             parameters = Parameters(session=bot.session, network_id=network.id).get(
                 station_id=station.id
             )
             print("population parameters db")
-            crud.create_parameters(next(get_db()), parameters, station_id=station.id)
+            crud.create_parameters(db, parameters, station_id=station.id)
