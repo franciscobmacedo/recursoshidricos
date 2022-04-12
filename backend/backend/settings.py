143761e038c68adb9a7ed16c6de3372c485641d6
Francisco Macedo <francisco.macedo@optimile.eu>
2022-04-12 08:31:56 +0100

    update static settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,4 +128,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+MEDIA_URL = "/mediafiles/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
