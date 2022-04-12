bcc0523576d83e31efe66563ba039146d328feb8
Francisco Macedo <francisco.macedo@optimile.eu>
2022-04-12 10:03:49 +0100

    register models with search

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -2,9 +2,21 @@ from django.contrib import admin
 
 from core import models
 
-# Register your models here.
-admin.site.register(models.Network)
-admin.site.register(models.Station)
-admin.site.register(models.Parameter)
+class NetworkAdmin(admin.ModelAdmin):
+    search_fields = ['nome', 'uid']
+
+class StationAdmin(admin.ModelAdmin):
+    search_fields = ['nome', 'uid', "codigo"]
+
+class ParameterAdmin(admin.ModelAdmin):
+    search_fields = ['nome', 'uid']
+
+
+class PSAAdmin(admin.ModelAdmin):
+    search_fields = ['station__uid', 'parameter__uid', 'station__nome', 'parameter__nome']
+
+admin.site.register(models.Network, NetworkAdmin)
+admin.site.register(models.Station, StationAdmin)
+admin.site.register(models.Parameter, ParameterAdmin)
 admin.site.register(models.Data)
-admin.site.register(models.PSA)
+admin.site.register(models.PSA, PSAAdmin)
