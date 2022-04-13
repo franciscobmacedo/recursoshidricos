af3a3806d97c52f47bc1ea1d3a6c2aeaef0aa3f9
Francisco Macedo <francisco.macedo@optimile.eu>
2022-04-13 11:32:44 +0100

    add temporary coming soon screen

---
+++ frontend/src/ComingSoon.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container fill-height fluid>
+        <v-row align="center" justify="center">
+          <div class="text-h1">Coming soon</div>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,14 @@
 import Vue from "vue";
-import App from "./App.vue";
+// import App from "./App.vue";
+import ComingSoon from "./ComingSoon.vue";
 import vuetify from "./plugins/vuetify";
 import "leaflet/dist/leaflet.css";
-import store from './store'
+import store from "./store";
 
 Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
   store,
-  render: (h) => h(App)
+  render: (h) => h(ComingSoon),
 }).$mount("#app");
