b196df2bbc6d53673330640eda175cf6311f5a73
Francisco Macedo <francisco.macedo@optimile.eu>
2022-04-15 14:59:58 +0100

    restart frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,8 @@
-import Vue from "vue";
-// import App from "./App.vue";
-import ComingSoon from "./ComingSoon.vue";
-import vuetify from "./plugins/vuetify";
-import "leaflet/dist/leaflet.css";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
-  vuetify,
-  store,
-  render: (h) => h(ComingSoon),
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app')
