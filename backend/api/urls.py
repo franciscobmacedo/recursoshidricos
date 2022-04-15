f88cf451282b8efb8aa8faaaa5fffae241cb1151
Francisco Macedo <francisco.macedo@optimile.eu>
2022-04-15 11:08:45 +0100

    fix: use redirect rather than creating 2 apis

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,6 +6,6 @@ from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("", RedirectView.as_view(url="latest/")),
-    path("latest/", api.urls),
+    path("latest/", RedirectView.as_view(url="v1/")),
     path("v1/", api.urls),
 ]
